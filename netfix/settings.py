381ad41becae7577be748e07cf50da8f48796a82
omrharbi <rharbu.omar@gmail.com>
2025-07-20 14:43:32 +0100

    disply pages

--- netfix/settings.py
+++ netfix/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # apps created
-    'main',
-    'services',
-    'users'
+    'main.apps.MainConfig',
+    'services.apps.ServicesConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static'),  # or os.path.join(BASE_DIR, 'static') for older Django
+]
+
+# For production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
